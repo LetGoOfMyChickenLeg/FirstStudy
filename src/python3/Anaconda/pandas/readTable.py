b9b544f9d91123e7f9d82e927ec3fd552434dd3e
liuruipeng <1034498754@qq.com>
2019-01-07 00:24:24 +0800

    change name

--- src/python3/Anaconda/pandas/readTable.py
+++ src/python3/Anaconda/pandas/readTable.py
@@ -4,5 +4,5 @@ from pandas import read_table
 A = Series([1, 2, 3], index = [1, 2, 3])
 print(A)
 
-df = read_table(r"DataOfreadtable.txt", sep=" *")
+df = read_table(r"DataOfreadtable.txt", sep="\s+")
 print(df.head())
